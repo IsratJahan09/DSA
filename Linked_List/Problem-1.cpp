fbcd6a308228241b662d516f48071de0bd941a95
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-01-09 12:26:22 +0600

    Update Problem-1.cpp

--- Linked_List/Problem-1.cpp
+++ Linked_List/Problem-1.cpp
@@ -1,1 +1,30 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+#typedef struct node Node;
+struct node
+{
+    int data;
+    Node *next;
+};
+
+int main(){
+    Node*head =NULL;
+    Node*second=NULL;
+    Node*third=NULL;
+  
+  head =(Node*)malloc(sizeof(Node));
+  second =(Node*)malloc(sizeof(Node));
+  third = (Node*)malloc(sizeof(Node));
+
+  head->data=2;
+  head->next=second;
+
+  second->data=3;
+  second->next=third;
+ 
+  third->data=6;
+  third->next=NULL;
+
+   //prtint your code using while loop 
+}
