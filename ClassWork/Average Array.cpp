cdf8805b9a60e11559ed79ef9342d161b2a157e4
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-01-29 10:57:44 +0600

    Create Average Array.cpp

---
+++ ClassWork/Average Array.cpp
@@ -0,0 +1,24 @@
+/*........Hard work and consistency is the only way to success........ */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=1005;
+//int d[N][N],pref[N][N];
+int main()
+{
+    // clock_t st= clock();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n; cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+     int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=a[i];
+    }
+    double average=(double)sum/n;
+    cout<<average<<endl;
+    return 0;
+}
