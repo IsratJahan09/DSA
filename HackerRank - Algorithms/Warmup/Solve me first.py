d0f70d10b249f5e86f5904ca7d4c9035912c8c3e
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-05-15 01:39:31 +0600

    Cpp and Pyhton Easy

---
+++ HackerRank - Algorithms/Warmup/Solve me first.py
@@ -0,0 +1,32 @@
+//python
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a+b;
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
+
+
+# cpp
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+    // Hint: Type return a+b; below
+ solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a+b;
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
